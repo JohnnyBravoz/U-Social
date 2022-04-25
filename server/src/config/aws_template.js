c488cc30fcdf2912ef2acc9a370c2c34357e36eb
JohnnyBravoz <70563099+JohnnyBravoz@users.noreply.github.com>
2022-04-25 07:25:53 -0600

    Update aws_template.js

--- server/src/config/aws_template.js
+++ server/src/config/aws_template.js
@@ -1,8 +1,8 @@
 let aws_keys = {
     s3: {
         region: 'us-east-2',
-        accessKeyId: "AKIAWRAU2HUJZHXIJJHN",
-        secretAccessKey: "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",
+        accessKeyId: "",
+        secretAccessKey: "",
         //apiVersion: '2006-03-01',
     },
     cognito:{
